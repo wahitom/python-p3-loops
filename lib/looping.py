efdad5cfaad6103995627e504bf61ba98b770060
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-22 23:48:36 +0300

    Completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1  
+        if i == 0:
+            print("Happy New Year!")
 
+happy_new_year()
+           
 def square_integers(int_list):
-    # code goes here!
-    pass
+    integers = int_list
+    new_integers = list()
+
+    for integer in integers:
+        new_integer = integer * integer 
+        new_integers.append(new_integer)
+    
+    return new_integers
+
+result = square_integers([1, 2, 3, 4, 5]) 
+print(result)     
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+
+    while num <= 100:
+        if (num % 3 == 0 and num % 5 == 0):
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+        num += 1
+
+fizzbuzz()
+        
